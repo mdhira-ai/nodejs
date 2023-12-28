3bc0acfbdfdfe54603e271f65d05e035adf04239
MD HABIBOR RAHMAN HIRA <mdhera891@gmail.com>
2023-12-28 15:50:32 -0500

    Refactor CORS middleware and update user disconnect handling

--- index.js
+++ index.js
@@ -6,22 +6,11 @@ const supabase = require('./supabase');
 
 const app = express();
 
+// Apply the cors middleware
+app.use(cors());
+
 const server = http.createServer(app);
 const io = socketIO(server);
-// app.use(cors({
-//     origin: 'https://project-1-get-every-user-interaction-of-the-ui.vercel.app'
-// }));
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://project-1-get-every-user-interaction-of-the-ui.vercel.app');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
-
-
-
-
 
 io.on('connection', (socket) => {
     console.log('A user connected', socket.id);
@@ -35,12 +24,11 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         // for last modified
-        const date = new Date()
-        const lasttime = date.toLocaleTimeString()
+        const date = new Date();
+        const lasttime = date.toLocaleTimeString();
         console.log('User disconnected', socket.id);
         // update disconnect user
         let id = socket.id;
-
 
         supabase.from('userinfo').update({
             lastModified: lasttime,
@@ -48,30 +36,17 @@ io.on('connection', (socket) => {
         }).eq(
             "socketid", id
         ).select().then((res) => {
-            console.log(res.statusText)
-
-        })
-
-
-
-
-
-
-
-
+            console.log(res.statusText);
+        });
     });
 });
-
 
 // Close the database connection when the server is stopped
 process.on('SIGINT', async () => {
     process.exit();
 });
 
-
 const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-
